03d23360b79e608686a6e59079a5af31f4a8228c
Pawel Hofman <pawel.hofman@procomsystem.pl>
2024-03-03 17:49:24 +0100

    obroty kartonów in progres, do zmiany jaszcze akcja drugiego przycisku

--- App/app.py
+++ App/app.py
@@ -2,11 +2,12 @@ import tkinter as tk
 from PIL import Image, ImageTk  # Dodane importy do obsługi obrazów
 
 class Karton:
-    def __init__(self, nazwa, wysokosc, szerokosc, dlugosc):
+    def __init__(self, nazwa, wysokosc, szerokosc, dlugosc, kat_obrotu):
         self.nazwa = nazwa
         self.wysokosc = wysokosc
         self.szerokosc = szerokosc
         self.dlugosc = dlugosc
+        self.kat_obrotu = kat_obrotu
 
 class Paleta:
     def __init__(self, szerokosc, dlugosc):
@@ -28,25 +29,41 @@ class App:
         self.canvas = tk.Canvas(master, width=self.paleta.szerokosc, height=self.paleta.dlugosc, bg="brown")
         self.canvas.pack()
 
-        self.canvas.bind("<Button-1>", self.dodaj_karton)
+        self.canvas.bind("<Button-1>", lambda event: self.dodaj_karton(event, dlugosc=150, szerokosc=50, wysokosc=50, kat_obrotu=19))
         self.canvas.bind("<B3-Motion>", self.przemiesc_karton)
         self.canvas.bind("<ButtonRelease-3>", self.zakoncz_przenoszenie)
         self.przenoszony_karton = None
 
-    def dodaj_karton(self, event):
+
+        
+    def dodaj_karton(self, event, szerokosc, dlugosc, wysokosc, kat_obrotu):
         nazwa_kartona = "Karton" + str(len(self.kartony) + 1)
-        szerokosc = 50
-        dlugosc = 50 
-        wysokosc = 70
-        nowy_karton = Karton(nazwa_kartona, wysokosc, szerokosc, dlugosc)
-        # Dodane wczytywanie obrazu i konwersja na format PhotoImage
-        obraz_kartona = Image.open("paleta_i_kartony_gui\\Graph\\karton.jpg")  # Wczytaj obraz kartonu
-        obraz_kartona = obraz_kartona.resize((nowy_karton.szerokosc, nowy_karton.dlugosc))
+        nowy_karton = Karton(nazwa_kartona, wysokosc, szerokosc, dlugosc, kat_obrotu)
+        obraz_kartona_poczatkowy = Image.open("paleta_i_kartony_gui\\Graph\\karton.jpg").convert("RGBA")
+
+        # Resizowanie obrazu
+        obraz_kartona_resize = obraz_kartona_poczatkowy.resize((nowy_karton.szerokosc, nowy_karton.dlugosc))
+        # Obrót obrazu
+        obraz_kartona_obrocony = obraz_kartona_resize.rotate(nowy_karton.kat_obrotu, expand=True)
+        
+        # Stwórz nowy obraz z kanałem alfa
+        nowy_szerokosc = max(obraz_kartona_obrocony.width, nowy_karton.szerokosc)
+        nowy_dlugosc = max(obraz_kartona_obrocony.height, nowy_karton.dlugosc)
+        obraz_kartona = Image.new("RGBA", (nowy_szerokosc, nowy_dlugosc), (0, 0, 0, 0))
+        
+        # Wklej obrócony obraz w środek nowego obrazu
+        x_offset = (nowy_szerokosc - obraz_kartona_obrocony.width) // 2
+        y_offset = (nowy_dlugosc - obraz_kartona_obrocony.height) // 2
+        obraz_kartona.paste(obraz_kartona_obrocony, (x_offset, y_offset), obraz_kartona_obrocony)
+
+        # obraz_kartona = obraz_kartona.resize((nowy_karton.szerokosc, nowy_karton.dlugosc))
         obraz_kartona = ImageTk.PhotoImage(obraz_kartona)
 
         karton_id = self.canvas.create_image(event.x, event.y, anchor=tk.NW, image=obraz_kartona)
         self.kartony.append((nowy_karton, karton_id, obraz_kartona))
         print("Dodano: " + nazwa_kartona + " o X: " + str(szerokosc) + " o Y: " + str(dlugosc))
+
+    
 
     def przemiesc_karton(self, event):
         self.zacznij_przenoszenie(event)
